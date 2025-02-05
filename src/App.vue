71f05d4a08f31b1374c38239e9cf0f5e8801b5f6
yamamoto <sys2870@icloud.com>
2025-02-05 10:06:17 +0900

    メッセージ画面の修正(スクロール、ヘッダー)、head>titleの変更

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <template>
   <div id="app">
-    <Navbar />
-    <router-view />
+    <Navbar v-if="isHeaderVisible" />
+    <router-view @toggle-header="toggleHeaderVisibility" />
     <footer class="border-top border-warning">
-      <p class="footer-text">© All rights reserved by Yakyuma-re</p>
+      <p class="footer-text">© All rights reserved by Yakyumare</p>
     </footer>
   </div>
 </template>
 
 <script setup>
 import Navbar from "./components/Navbar.vue";
+import { useRoute } from "vue-router";
+import { watch, ref } from "vue";
+
+const route = useRoute();
+const isHeaderVisible = ref(true);
+// スクロールを禁止するページのパス
+const noScrollPages = ["/message"];
+
+const toggleHeaderVisibility = (visible) => {
+  isHeaderVisible.value = visible;
+};
+
+watch(route, (newRoute) => {
+  if (noScrollPages.includes(newRoute.path)) {
+    document.documentElement.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
+  } else {
+    document.documentElement.style.overflow = "auto";
+    document.body.style.overflow = "auto";
+  }
+
+  // head titleをルートごとに変更する
+  if (newRoute.meta.title) {
+    let setTitle = newRoute.meta.title + " | やきゅま～れ";
+    document.title = setTitle;
+  } else {
+    document.title = "やきゅまーれ";
+  }
+
+  // メタタグのdescription設定処理
+  if (newRoute.meta.desc) {
+    document
+      .querySelector("meta[name='description']")
+      .setAttribute("content", newRoute.meta.desc);
+  } else {
+    document
+      .querySelector("meta[name='description']")
+      .setAttribute(
+        "content",
+        "草野球専門の総合掲示板『やきゅま～れ』は、試合募集、メンバー募集、助っ人募集など、草野球に関する情報を簡単に共有できるプラットフォームです。野球好きの仲間と試合や練習の予定を調整するのに最適なコミュニティです。"
+      );
+  }
+
+  // **ページ遷移時にスクロールをリセット**
+  window.scrollTo(0, 0);
+});
 </script>
 
 <style>
--- src/router/recruitment.ts
+++ src/router/recruitment.ts
@@ -4,6 +4,7 @@ export default {
       path: "/recruitment/list",
       name: "recruitmentList",
       component: () => import("@/views/Recruitment/RecruitmentInquiry.vue"),
+      meta: { title: "募集一覧" },
     },
     {
       path: "/recruitment/:id",
@@ -14,13 +15,13 @@ export default {
       path: "/recruitment/create",
       name: "RecruitmentCreate",
       component: () => import("@/views/Recruitment/RecruitmentForm.vue"),
-      meta: { requiresAuth: true },
+      meta: { requiresAuth: true, title: "募集新規作成" },
     },
     {
       path: "/recruitment/edit/:id",
       name: "RecruitmentEdit",
       component: () => import("@/views/Recruitment/RecruitmentForm.vue"),
-      meta: { requiresAuth: true },
+      meta: { requiresAuth: true, title: "募集編集" },
     },
   ],
 };
